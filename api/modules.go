d83c5430db30df26de43b8aec91be1840ec88997
Vitor Emanoel <vitore88@gmail.com>
2020-07-09 23:02:38 -0300

    Added employers router

--- api/modules.go
+++ api/modules.go
@@ -4,8 +4,9 @@ import "jea-api/models"
 
 // Modules list with all API and routers
 type Modules struct {
-	Company models.Company `router:"/companies"`
-	Client  models.Client  `router:"/clients"`
-	User    models.User    `router:"/users"`
-	Group   models.Group   `router:"/groups"`
+	Company 	models.Company 	`router:"/companies"`
+	Client  	models.Client  	`router:"/clients"`
+	User    	models.User    	`router:"/users"`
+	Group   	models.Group   	`router:"/groups"`
+	Employer 	models.Employer	`router:"/employers"`
 }
