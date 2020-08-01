73c4f76fa73fa5ddab1ff1ecc538a0bc8fdd28df
Vitor Emanoel <vitore88@gmail.com>
2020-08-01 12:17:03 -0300

    Added workload and companyID for employer

--- models/employer.go
+++ models/employer.go
@@ -7,8 +7,11 @@ type Employer struct {
 	CPF			string		`json:"cpf"`
 	RG			string		`json:"rg"`
 	Type		int			`json:"type"`
+	Workload	int			`json:"workload"`
 	Address
 	Sales		[]*Sale		`json:"sales,omitempty" gorm:"foreignkey:SellerID;association_foreignkey:ID;association_autocreate:false;association_autoupdate:false"`
+	CompanyID	int64		`json:"-"`
+	Company		*Company	`json:"company,omitempty" gorm:"foreignkey:CompanyID;association_foreignkey:ID;association_autocreate:false;association_autoupdate:false"`
 }
 
 func (e *Employer) GetFilters() Filters {
